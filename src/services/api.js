44a7d9db4fc69be766e9e5d30707819c89875ef0
Edivaldo Júnior <55596318+EdivaldoJunior01@users.noreply.github.com>
2021-11-24 15:57:32 -0300

    Update api.js

--- src/services/api.js
+++ src/services/api.js
@@ -2,6 +2,7 @@ import axios from "axios";
 
 const api =axios.create({
     baseURL:'https://crud-simple-react-js.vercel.app/api'
+    // ou  baseURL: 'http://localhost:3000/api'caso esteja rodando local
 })
 
 export default api
